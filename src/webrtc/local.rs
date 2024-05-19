4b193035525fc49177097690f6f170c92baf9843
Shrey Joshi <shreyjoshi2004@gmail.com>
2024-05-18 22:11:41 -0500

    add local webrtc example

--- src/webrtc/local.rs
+++ src/webrtc/local.rs
@@ -1,40 +1,85 @@
-use anyhow::Result;
+use std::io::Write;
+
 use just_webrtc::{
     types::{ICECandidate, SessionDescription},
     DataChannelExt, PeerConnectionExt, SimpleLocalPeerConnection,
 };
 
-async fn run_local_peer() -> Result<()> {
+#[tokio::main]
+async fn main() {
     // create simple local peer connection with unordered data channel
-    let mut local_peer_connection = SimpleLocalPeerConnection::build(false).await?;
+    let mut local_peer_connection = SimpleLocalPeerConnection::build(false).await.unwrap();
 
     // output offer and candidates for remote peer
     let offer = local_peer_connection.get_local_description().await.unwrap();
-    let candidates = local_peer_connection.collect_ice_candidates().await?;
+    let candidates = local_peer_connection
+        .collect_ice_candidates()
+        .await
+        .unwrap();
 
     // ... send the offer and the candidates to Peer B via external signalling implementation ...
     let signalling = (offer, candidates);
+
+    // write it to file
+    let mut file = std::fs::File::create("signalling.txt").unwrap();
+    let data = bincode::serialize(&signalling).unwrap();
+    // data sent to firebase rtdb by turning vec of bytes into string that is b1-b2-b3-...-bn, then read back in on the other side
+    file.write_all(&data).unwrap();
+
+    println!("Generated signaling offer and wrote to file. Press enter once answer.txt has been created.");
+
+    // pause until user presses enter
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    // then read in the file answer.txt
+    let file = std::fs::File::open("answer.txt").unwrap();
+    let signalling: (SessionDescription, Vec<ICECandidate>) =
+        bincode::deserialize_from(file).unwrap();
 
     // ... receive the answer and candidates from Peer B via external signalling implementation ...
     let (answer, candidates) = signalling;
 
     // update local peer from received answer and candidates
-    local_peer_connection.set_remote_description(answer).await?;
-    local_peer_connection.add_ice_candidates(candidates).await?;
+    local_peer_connection
+        .set_remote_description(answer)
+        .await
+        .unwrap();
+    local_peer_connection
+        .add_ice_candidates(candidates)
+        .await
+        .unwrap();
+
+    println!("Received signaling answer and candidates. Waiting for connection to complete.");
 
     // local signalling is complete! we can now wait for a complete connection
     local_peer_connection.wait_peer_connected().await;
 
+    println!("Peer connection complete!");
+
     // receive data channel from local peer
     let mut local_channel = local_peer_connection.receive_channel().await.unwrap();
+
     // wait for data channels to be ready
     local_channel.wait_ready().await;
 
-    // send data to remote (answerer)
-    local_channel
-        .send(&bytes::Bytes::from("hello remote!"))
-        .await?;
+    println!("Data channel ready!");
+
     // recv data from remote (answerer)
-    let recv = local_channel.receive().await.unwrap();
-    assert_eq!(&recv, "hello local!");
+    loop {
+        // send data to remote (answerer)
+        local_channel
+            .send(&bytes::Bytes::from("hello remote!"))
+            .await
+            .unwrap();
+
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        let recv = local_channel.receive().await.unwrap();
+
+        println!("Received: {:?}", recv);
+
+        // pause for 1 second
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
--- src/webrtc/remote.rs
+++ src/webrtc/remote.rs
@@ -1,42 +1,73 @@
-use anyhow::Result;
+use std::io::Write;
+
 use just_webrtc::{
     types::{ICECandidate, SessionDescription},
     DataChannelExt, PeerConnectionExt, SimpleRemotePeerConnection,
 };
 
-async fn run_remote_peer(offer: SessionDescription, candidates: Vec<ICECandidate>) -> Result<()> {
+#[tokio::main]
+async fn main() {
     // ... receive the offer and the candidates from Peer A via external signalling implementation ...
+    // read from file
+    let file = std::fs::File::open("signalling.txt").expect("something failed");
+    let signalling: (SessionDescription, Vec<ICECandidate>) =
+        bincode::deserialize_from(file).expect("something failed");
+    let (offer, candidates) = signalling;
 
     // create simple remote peer connection from received offer and candidates
-    let mut remote_peer_connection = SimpleRemotePeerConnection::build(offer).await?;
+    let mut remote_peer_connection = SimpleRemotePeerConnection::build(offer)
+        .await
+        .expect("something failed");
     remote_peer_connection
         .add_ice_candidates(candidates)
-        .await?;
+        .await
+        .expect("something failed");
     // output answer and candidates for local peer
     let answer = remote_peer_connection
         .get_local_description()
         .await
-        .unwrap();
-    let candidates = remote_peer_connection.collect_ice_candidates().await?;
+        .expect("something failed");
+    let candidates = remote_peer_connection
+        .collect_ice_candidates()
+        .await
+        .expect("something failed");
 
     // ... send the answer and the candidates back to Peer A via external signalling implementation ...
     let _signalling = (answer, candidates);
 
+    // write it to file
+    let mut file = std::fs::File::create("answer.txt").expect("something failed");
+    let data = bincode::serialize(&_signalling).expect("something failed");
+    file.write_all(&data).expect("something failed");
+
     // remote signalling is complete! we can now wait for a complete connection
     remote_peer_connection.wait_peer_connected().await;
 
+    println!("Peer connection complete!");
+
     // receive data channel from local and remote peers
-    let mut remote_channel = remote_peer_connection.receive_channel().await.unwrap();
+    let mut remote_channel = remote_peer_connection
+        .receive_channel()
+        .await
+        .expect("Failed to receive data channel!");
     // wait for data channels to be ready
     remote_channel.wait_ready().await;
 
-    // send/recv data from local (offerer) to remote (answerer)
-    let recv = remote_channel.receive().await.unwrap();
-    assert_eq!(&recv, "hello remote!");
-    // send/recv data from remote (answerer) to local (offerer)
-    remote_channel
-        .send(&bytes::Bytes::from("hello local!"))
-        .await?;
+    println!("Data channel ready!");
 
-    Ok(())
+    // send/recv data from local (offerer) to remote (answerer)
+    loop {
+        let recv = remote_channel.receive().await.expect("something failed");
+        println!("Received: {:?}", recv);
+
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        remote_channel
+            .send(&bytes::Bytes::from("hello local!"))
+            .await
+            .expect("Failed to send data to local peer!");
+
+        // pause for 1 second
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
